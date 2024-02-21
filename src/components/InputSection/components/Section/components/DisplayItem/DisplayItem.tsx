45ce96bcc2c934fb593d413b29b5bdc83538f356
Gary_Yao <ygy3389@gmail.com>
2024-02-22 00:44:29 +1100

    rename section

---
+++ src/components/InputSection/components/Section/components/DisplayItem/DisplayItem.tsx
@@ -0,0 +1,38 @@
+import Quill from '@/components/Quill'
+import { Fields } from '@/types/Fields'
+import titleGenerator from '@/utils/titleGenerator'
+import dayjs from 'dayjs'
+import { FC } from 'react'
+
+interface DisplayItemProps {
+  field: Fields
+  openEdit: () => void
+}
+const DisplayItem: FC<DisplayItemProps> = (props) => {
+  const { field, openEdit } = props
+  return (
+    <div className="w-full">
+      <div className="flex items-center gap-10 px-2">
+        <div>
+          <div className="text-xl font-semibold">{titleGenerator(field)}</div>
+          {field.data.startDate?.value && (
+            <p>{`${dayjs(field.data.startDate?.value).format('MM/YYYY')} - ${
+              field.data.endDate?.value
+                ? dayjs(field.data.endDate?.value).format('MM/YYYY')
+                : 'current'
+            }`}</p>
+          )}
+        </div>
+        <img
+          className="w-6 h-6"
+          src="/src/assets/edit.svg"
+          alt="edit"
+          onClick={openEdit}
+        />
+      </div>
+      <Quill value={field.description} readOnly={!field.editMode} />
+    </div>
+  )
+}
+
+export default DisplayItem
